7774eacf25319b69223344e3ff9216fa2a63d8e9
ravikishorec <ravikishore.chavana@gmail.com>
2024-09-12 02:41:04 +0530

    this is my third commit of this project!

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -5,22 +5,22 @@ import random
 
 # Create your views here.
 def home(request):
-    return render(request ,'/generator/home.html')
+    return render(request ,'home.html')
 def about(request):
-    return render(request ,'/generator/about.html')
+    return render(request,'about.html')
 def password(request):
 
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    if request.GET.get('uppercase'):
+    if request.GET.get('Uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
-    if request.GET.get('special'):
+    if request.GET.get('Special'):
         characters.extend(list('@#$%^&*()_+!'))
-    if request.GET.get('numbers'):
+    if request.GET.get('Numbers'):
         characters.extend(list('0123456789'))
     length=int(request.GET.get('length',12))
     thepassword = ''
     for x in range(length):
         thepassword += random.choice(characters)
 
-    return render(request,'generator/password.html',{'password':thepassword})
+    return render(request,'password.html',{'password':thepassword})
